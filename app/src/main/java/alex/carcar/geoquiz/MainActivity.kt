6ecd57c8bd88d153ddf8f9f2066c9f9e6e59e3dc
acarcar <alexcarcar@yahoo.com>
2020-03-14 10:59:00 -0700

    Initial commit

---
+++ app/src/main/java/alex/carcar/geoquiz/MainActivity.kt
@@ -0,0 +1,12 @@
+package alex.carcar.geoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
