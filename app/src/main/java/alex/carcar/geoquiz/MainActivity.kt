fcbd38f43ce5219a9d641fa7bf2a751c4091f37c
acarcar <alexcarcar@yahoo.com>
2020-04-05 12:09:09 -0700

    Added logging to track 6 Activity lifecycle methods:  onCreate/onDestroy,  onStart/onStop,  onResume/onPause

--- app/src/main/java/alex/carcar/geoquiz/MainActivity.kt
+++ app/src/main/java/alex/carcar/geoquiz/MainActivity.kt
@@ -1,11 +1,14 @@
 package alex.carcar.geoquiz
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,8 +29,10 @@ class MainActivity : AppCompatActivity() {
 
     private var currentIndex = 0
 
+    // --- Nonexistent State
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -65,6 +70,35 @@ class MainActivity : AppCompatActivity() {
 
         updateQuestion()
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
+    // --- Stopped State
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    // --- Paused State
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    // --- Resumed State
 
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
